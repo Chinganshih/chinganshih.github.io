3d405057a17a354679c1994c992db41eaba49c71
Chinganshih <anna0935537079@gmail.com>
2022-04-15 09:50:30 -0400

    add information dialog after submit

--- js/app.js
+++ js/app.js
@@ -63,7 +63,7 @@ function validateInput(e) {
 
 // 1.	Create an event handler to run when the page is loaded.
 window.onload = function() {
-    var formDate = {};
+    var formData = {};
     var contact = document.querySelector(".contact");
     var fname = document.querySelector("#fname");
     var lname = document.querySelector("#lname");
@@ -124,6 +124,7 @@ window.onload = function() {
     })
 
     submit.addEventListener("click", function(e) {
+        var hourrateEpy = 0;
         var hourlyinput = document.querySelector('#hourlyinput');
         var url = "https://httpbin.org/post?key=formdata";
         var xhr = new XMLHttpRequest();
@@ -132,19 +133,29 @@ window.onload = function() {
         if (hourlyinput) {
             const isInvalid = hourlyinput.value === "" || !isFinite(hourlyinput.value);
             hourlyinput.classList.toggle("invalid", isInvalid);
+            (isInvalid) ? hourrateEpy = 1: 0;
         }
         const isInvalid = validateInput({ target: fname }) | validateInput({ target: lname }) | validateInput({ target: email }) | validateInput({ target: address }) | validateInput({ target: city }) |
             validateInput({ target: postalcode }) | validateInput({ target: question }) | validateInput({ target: other });
 
-        if (!isInvalid) {
-            formDate.fname = fname.value;
-            formDate.lname = lname.value;
-            formDate.email = email.value;
-            formDate.address = address.value;
-            formDate.city = city.value;
-            formDate.postalcode = postalcode.value;
-            formDate.question = question.value;
-            formDate.other = other.value;
+        if (!isInvalid && !hourrateEpy) {
+            formData.fname = fname.value;
+            formData.lname = lname.value;
+            formData.email = email.value;
+            formData.address = address.value;
+            formData.city = city.value;
+            formData.postalcode = postalcode.value;
+            formData.other = other.value;
+
+            if (question.checked) {
+                console.log(question.value);
+                formData.question_about = question.value;
+            } else if (comment.checked) {
+                formData.question_about = comment.value;
+            } else {
+                formData.question_about = hiring.value;
+                formData.hourRate = hourlyinput.value;
+            }
 
             xhr.onreadystatechange = () => {
                 if (xhr.readyState == 4 && xhr.status == 200) {
@@ -152,9 +163,31 @@ window.onload = function() {
                 }
             }
             xhr.open("POST", url);
-            xhr.send(JSON.stringify(formDate));
-            console.log(formDate);
-        }
+            xhr.send(JSON.stringify(formData));
+            console.log(formData.question_about);
+
+            window.confirm("Please check your information below.\n" +
+                "First Name: " + fname.value + "\n" +
+                "Last Name: " + lname.value + "\n" +
+                "Email: " + email.value + "\n" +
+                "Address: " + address.value + "\n" +
+                "City: " + city.value + "\n" +
+                "Postal Code: " + postalcode.value + "\n" +
+                "Question About: " + formData.question_about + "\n" +
+                "Other: " + other.value + "\n" +
+                "Submit successfully! Thank you for your feedback!");
+
+
+            fname.value = "";
+            lname.value = "";
+            email.value = "";
+            address.value = "";
+            city.value = "";
+            postalcode.value = "";
+            question.value = "";
+            other.value = "";
+            if (hourlyinput) hourlyinput.value = "";
+        } else alert("Invalid information, Please check again before you submit!");
 
     })
 };
